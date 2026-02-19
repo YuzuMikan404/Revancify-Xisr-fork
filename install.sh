7cc5a9ff73432fd9142bd3126a9ef1119fccf623
YuzuMikan <pangohan31@outlook.jp>
2026-02-19 17:10:06 +0900

    Update download URL for xisr in install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 [ -z "$TERMUX_VERSION" ] && echo -e "Termux not detected !!" && exit 1
 BIN="$PREFIX/bin/xisr"
-curl -sL "https://github.com/Xisrr1/Revancify-Xisr/raw/refs/heads/main/xisr" -o "$BIN"
+curl -sL "https://github.com/YuzuMikan404/Revancify-Xisr-fork/raw/refs/heads/main/xisr" -o "$BIN"
 [ -e "$BIN" ] && chmod +x "$BIN" && "$BIN"
